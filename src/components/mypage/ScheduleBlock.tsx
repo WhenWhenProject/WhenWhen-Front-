cee66758ea034538ca8ce98444f2d5ffa38a3a1e
Sukyeong-hyu <81177665+Sukyeong-hyu@users.noreply.github.com>
2022-02-14 08:31:28 +0900

    feat: 라우터로 페이지 생성 및 주요 컴포넌트 분류 및 생성 (#6)

---
+++ src/components/mypage/ScheduleBlock.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// 컴포넌트: 마이페이지에 담기는 스케줄 각각의 블록
+// 확정된 일정은 초록색, 확정되지 않은 일정은 남색
+const ScheduleBlock = () => {
+  return <div>ScheduleBlock</div>;
+};
+
+export default ScheduleBlock;
---
+++ src/pages/ScheduleResult/ScheduleResult.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// 모두가 적었으면 최종 결과 페이지, 한 명이라도 적지 않은 사람이 있으면 중간 결과 페이지
+const ScheduleResult = () => {
+  return <div>ScheduleResult</div>;
+};
+
+export default ScheduleResult;
